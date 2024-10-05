528baa691de5f7ebd12757ff264ae64947beb2a7
patoomoretti <patriciomoretti@hotmail.com>
2024-10-05 20:11:55 +0000

    Punto_A.sh

---
+++ RTA_SCRIPT_Examen_20241005/Punto_A.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Creando carpeta Examen UTN"
+
+sudo mkdir -p /Examenes-UTN/{alumno_1/parcial_{1..3},alumno_2/parcial_{1..3},alumno_3/parcial_{1..3},profesores}
+
+echo "Carpetas creadas!"
